d91fb095ed6ec058c7b67284d7a9e65f1bf03876
Alejandro Recio <alejandrorecio.r@gmail.com>
2022-11-29 12:41:20 +0100

    Fix

--- sort.py
+++ sort.py
@@ -1,24 +1,31 @@
 import os
 import pathlib
 
-root = 'example_folder/'
+sorter_file_name = os.path.basename(__file__)
+root = os.getcwd() + "/"
 
 for entry in os.scandir(root):
-   if entry.is_dir():
+
+    if entry.name == sorter_file_name:
+        continue
+    if entry.is_dir():
         typ = 'dir'
-   elif entry.is_file():
+    elif entry.is_file():
         typ = 'file'
         file_extension = pathlib.Path(entry.name).suffix
-        path = root + file_extension.split('.')[1]
-        is_exists = os.path.exists(path)
-        if not is_exists:
-            os.mkdir(path)
-        os.rename(entry.path, path + '/' + entry.name)
-   elif entry.is_symlink():
+        if file_extension != '':
+            path = root + file_extension.split('.')[1]
+            is_exists = os.path.exists(path)
+            if not is_exists:
+                print("Create folder: " + path)
+                os.mkdir(path)
+            os.rename(entry.path, path + '/' + entry.name)
+            print("Moving: " + entry.path + ' - ' + path + '/' + entry.name)
+    elif entry.is_symlink():
         typ = 'link'
-   else:
+    else:
         typ = 'unknown'
-   print('{name} {typ}'.format(
-        name=entry.name,
-        typ=typ,
-   ))
+#    print('{name} {typ}'.format(
+#         name=entry.name,
+#         typ=typ,
+#    ))

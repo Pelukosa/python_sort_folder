0eafd759f619f43ad05b7309c696094a4d70dc4e
Alejandro Recio <alejandrorecio.r@gmail.com>
2022-11-29 18:58:04 +0000

    Create folders

--- sort.py
+++ sort.py
@@ -3,22 +3,43 @@ import pathlib
 
 root = 'example_folder/'
 
+images = ['jpg', 'png', 'gif', 'jpeg']
+video = ['mp4', 'mov', 'wmv', 'flv', 'avi']
+doc = ['doc', 'docx', 'pdf']
+sheet = ['xls', 'xlsx']
+install = ['exe']
+compressed = ['zip', 'rar']
+
+def getFolderNameByExtension(extension):
+     if extension in images:
+          return "Images"
+     elif extension in video:
+          return "Videos"
+     elif extension in doc:
+          return "Documents"
+     elif extension in sheet:
+          return "Sheets"
+     elif extension in install:
+          return "Installables"
+     elif extension in compressed:
+          return "Compressed"
+     else:
+          return "Unknown"
+
+
 for entry in os.scandir(root):
-   if entry.is_dir():
-        typ = 'dir'
-   elif entry.is_file():
-        typ = 'file'
-        file_extension = pathlib.Path(entry.name).suffix
-        path = root + file_extension.split('.')[1]
-        is_exists = os.path.exists(path)
-        if not is_exists:
-            os.mkdir(path)
-        os.rename(entry.path, path + '/' + entry.name)
-   elif entry.is_symlink():
-        typ = 'link'
-   else:
-        typ = 'unknown'
-   print('{name} {typ}'.format(
-        name=entry.name,
-        typ=typ,
-   ))
+     if entry.is_dir():
+          typ = 'dir'
+     elif entry.is_file():
+          file_extension = pathlib.Path(entry.name).suffix
+          file_extension = file_extension.split('.')[1]
+          folder = getFolderNameByExtension(file_extension)
+          path = root + folder
+          is_exists = os.path.exists(path)
+          if not is_exists:
+               os.mkdir(path)
+          os.rename(entry.path, path + '/' + entry.name)
+     elif entry.is_symlink():
+          typ = 'link'
+     else:
+          typ = 'unknown'
